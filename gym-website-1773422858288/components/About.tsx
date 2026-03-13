fb9f3ba915074ac35898edc970fc855971fccda9
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:57:41 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422858288/components/About.tsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <section className="py-12">
+      <div className="container mx-auto p-4">
+        <h2 className="text-3xl font-bold">About Us</h2>
+        <p>
+          We are a team of passionate fitness enthusiasts dedicated to helping
+          you achieve your fitness goals.
+        </p>
+        <p>
+          Our state-of-the-art facility offers a variety of equipment and
+          classes to suit all fitness levels.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
