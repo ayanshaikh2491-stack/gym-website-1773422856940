daff248d20ca3361872e695a4f9c7e17fb36ccdc
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:57:45 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422858288/pages/services.tsx
@@ -0,0 +1,17 @@
+import Layout from '../layout';
+
+function ServicesPage() {
+  return (
+    <Layout>
+      <h1>Services</h1>
+      <p>We offer a variety of services to help you achieve your fitness goals, including:</p>
+      <ul>
+        <li>Personal training</li>
+        <li>Group fitness classes</li>
+        <li>Nutrition planning</li>
+      </ul>
+    </Layout>
+  );
+}
+
+export default ServicesPage;
